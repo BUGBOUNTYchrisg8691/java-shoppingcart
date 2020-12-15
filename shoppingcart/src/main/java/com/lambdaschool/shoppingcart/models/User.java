f3db5c47716439468b5793c14deaafee10c7baef
Christopher Girvin <bugbounty.chrisg8691@gmail.com>
2020-12-15 18:09:15 -0500

    deps added to pom; data.sql user pass's encrypted; AuthServCon, ResServCon, SecCon, and UDetServImpl done

--- shoppingcart/src/main/java/com/lambdaschool/shoppingcart/models/User.java
+++ shoppingcart/src/main/java/com/lambdaschool/shoppingcart/models/User.java
@@ -1,12 +1,13 @@
 package com.lambdaschool.shoppingcart.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * The entity allowing interaction with the users table
@@ -212,4 +213,18 @@ public class User
     {
         this.carts = carts;
     }
+
+    @JsonIgnore
+	public List<SimpleGrantedAuthority> getAuthority()
+	{
+	    List<SimpleGrantedAuthority> rtnList = new ArrayList<>();
+        
+        for (UserRoles r : this.roles)
+        {
+            String role = "ROLE_" + r.getRole().getName().toUpperCase();
+            rtnList.add(new SimpleGrantedAuthority(role));
+        }
+        
+        return rtnList;
+	}
 }
